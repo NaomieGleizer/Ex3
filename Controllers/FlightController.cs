d7d5064d36e914607ae0f66e68ddc686c57d2171
Naomie Gleizer <naomie.gleizer@gmail.com>
2019-06-04 12:19:11 +0300

    ex3

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Mvc;
+using Ajax_Minimal.Models;
+using System.IO;
+using System.Reflection;
+using System.Web.Configuration;
+
+namespace Ajax_Minimal.Controllers
+{
+    public class FlightController : Controller
+    {
+        // GET: Flight
+
+        private static CommandsModel model = new CommandsModel();
+        private string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToString(), "App_Data/flight1.txt");
+        private static StreamReader reader= null;
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PositionOrLoad(string s, int num)
+        {
+            IPAddress ip;
+            if (IPAddress.TryParse(s, out ip))
+            {
+                return Position(s, num);
+            }
+            return Load(s, num);
+        }
+
+        [HttpGet]
+        public ActionResult map(string ip, int port, int seconds)
+        {
+            ClosePreviousView();
+
+            model.Connect();
+            Session["time"] = 1.0 / seconds;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Position(string ip, int port)
+        {
+            ClosePreviousView();
+            model.Connect();
+
+            return View("Position");
+        }
+
+        public void ClosePreviousView()
+        {
+
+            if (reader != null)
+            {
+                reader.Close();
+                reader = null;
+            }
+            if (!model.GetToStop())
+            {
+                model.Disconnect();
+                model.SetFlagToFalse();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Save(string ip, int port, int perSeconds, int duration, string file)
+        {
+            ClosePreviousView();
+       
+            model.Connect();
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            FileStream flight= System.IO.File.Create(path);
+            flight.Close();
+            Session["perSec"] = 1.0 / perSeconds;
+            Session["duration"] = duration;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Load(string file, int seconds)
+        {
+            ClosePreviousView();
+
+            Session["perSec"] = 1.0 / seconds;
+            reader = new StreamReader(path);
+            return View("Load");
+        }
+
+        [HttpGet]
+        public JsonResult GetLanLot()
+        {
+            double lon;
+            double lat;
+            model.SetCommands(1);
+
+            do
+            {
+                lon = model.Longtitude;
+                lat = model.Latitude;
+            } while (!model.GetFlagFinish() && !model.GetToStop());
+            if (model.GetToStop())
+            {
+                return null;
+            }
+            model.SetFlagToFalse();
+            return Json(new { x = lon, y = lat }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult SaveValuesOfPlan()
+        {
+            double lon, lat, alt, speed;
+            model.SetCommands(2);
+            do
+            {
+                lon = model.Longtitude;
+                lat = model.Latitude;
+                speed = model.Speed;
+                alt = model.Altitude;
+            } while (!model.GetFlagFinish() && !model.GetToStop());
+            if (model.GetToStop())
+            {
+                return null;
+            }
+            model.SetFlagToFalse();
+            string[] lines = { lon.ToString(), lat.ToString(), speed.ToString(), alt.ToString() };
+            //string filePath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, @"App_Data", @"flight1.txt");
+            System.IO.File.AppendAllLines(path, lines);
+            return Json(new { x = lon, y = lat }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public JsonResult LoadValuesFromFile()
+        {
+            string lon = reader.ReadLine();
+            if (lon == null)
+            {
+                return Json(new object[] { new object() }, JsonRequestBehavior.AllowGet); ;
+            }
+            string lat = reader.ReadLine();
+            string speed = reader.ReadLine();
+            string alt = reader.ReadLine();
+            return Json(new { x = Convert.ToDouble(lon), y = Convert.ToDouble(lat)
+                , z = Convert.ToDouble(speed), w = Convert.ToDouble(alt) }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/CommandModel.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Ajax_Minimal.Models
+{
+
+    /// <summary>
+    /// CommandsModel : model of Auto Pilot, and Joystick. 
+    /// Connected to the flight simulator and send it commands. 
+    /// </summary>
+    public class CommandsModel 
+    {
+        private bool stop = true;
+        private int commands = 0;
+        private bool flagFinish;
+        private string ip = "127.0.0.1";
+        private int port = 5402;
+        private string getLat = "get /position/latitude-deg\r\n";
+        private string getLon = "get /position/longitude-deg\r\n";
+        private string getAlt = "get /instrumentation/altimeter/indicated-altitude-ft\r\n";
+        private string getSpeed = "get /instrumentation/airspeed-indicator/indicated-speed-kt\r\n";
+        //private Mutex mtx;
+
+        public CommandsModel()
+        {
+
+        }
+
+        public double Longtitude
+        {
+            get; set;
+        }
+
+        public double Latitude
+        {
+            get; set;
+        }
+
+        public double Altitude
+        {
+            get; set;
+        }
+        public double Speed
+        {
+            get; set;
+        }
+
+        public void SetCommands(int val)
+        {
+            commands = val;
+        }
+
+        public void SetFlagToFalse()
+        {
+            flagFinish = false;
+        }
+
+        public bool GetFlagFinish()
+        {
+            return flagFinish;
+        }
+
+        public bool GetToStop()
+        {
+            return stop;
+        }
+
+        /// <summary>
+        /// OpenSocket : opens the client socket and connects to the simulator.
+        /// </summary>
+        public void OpenSocket()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+            using (NetworkStream stream = client.GetStream())
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                StreamReader sr = new StreamReader(stream);
+                string input;
+                string[] values;
+                // while not stop connection, send given commands to the simulator 
+                while (!stop)
+                {
+                    Byte[] data = new Byte[100];
+                    // send every command to the simulator
+                    if (commands == 1 || commands == 2)
+                    {
+                        flagFinish = false;
+                        data = Encoding.ASCII.GetBytes(getLon);
+                        stream.Write(data, 0, data.Length); // write to simulator
+                        input = sr.ReadLine();
+                        values = input.Split('\'');
+                        Longtitude = Convert.ToDouble(values[1]);
+                        data = Encoding.ASCII.GetBytes(getLat);
+                        stream.Write(data, 0, data.Length); // write to simulator
+                        input = sr.ReadLine();
+                        values = input.Split('\'');
+                        Latitude = Convert.ToDouble(values[1]);
+                        if (commands == 2)
+                        {
+                            data = Encoding.ASCII.GetBytes(getSpeed);
+                            stream.Write(data, 0, data.Length); // write to simulator
+                            input = sr.ReadLine();
+                            values = input.Split('\'');
+                            Speed = Convert.ToDouble(values[1]);
+                            data = Encoding.ASCII.GetBytes(getAlt);
+                            stream.Write(data, 0, data.Length); // write to simulator
+                            input = sr.ReadLine();
+                            values = input.Split('\'');
+                            Altitude = Convert.ToDouble(values[1]);
+                        }
+                        commands = 0;
+                        flagFinish = true;
+                    }
+                }
+            }
+            client.Close();
+        }
+
+
+        /// <summary>
+        /// Connect : opens a task that connects and sends given commands to the simulator
+        /// </summary>
+        public void Connect()
+        {
+            stop = false;
+            Task t = new Task(() => { OpenSocket(); });
+
+            t.Start();
+        }
+
+
+        /// <summary>
+        /// Disconnect : disconnect the simulator
+        /// </summary>
+        public void Disconnect()
+        {
+            stop = true;
+        }
+
+    }
+    
+}
